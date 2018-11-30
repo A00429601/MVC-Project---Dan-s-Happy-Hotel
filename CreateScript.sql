3b42c0c7caee6ca2879dd270416be9e9f2b5833b
kejifasuyi <43154234+kejifasuyi@users.noreply.github.com>
2018-11-30 01:50:16 +0000

    UpdateCreateScript

--- CreateScript.sql
+++ CreateScript.sql
@@ -1,7 +1,7 @@
 USE [RentalProject]
 GO
 
-/****** Object:  Table [dbo].[UserAccount]    Script Date: 11/30/2018 12:48:59 AM ******/
+/****** Object:  Table [dbo].[UserAccount]    Script Date: 11/30/2018 1:47:32 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -25,19 +25,19 @@ GO
 USE [RentalProject]
 GO
 
-/****** Object:  Table [dbo].[Cars]    Script Date: 11/30/2018 12:49:05 AM ******/
+/****** Object:  Table [dbo].[Car]    Script Date: 11/30/2018 1:47:38 AM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[Cars](
+CREATE TABLE [dbo].[Car](
 	[carID] [nvarchar](50) NOT NULL,
 	[carName] [nvarchar](max) NULL,
 	[carType] [nvarchar](max) NULL,
 	[carImage] [nvarchar](max) NULL,
- CONSTRAINT [PK_Cars] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_Car] PRIMARY KEY CLUSTERED 
 (
 	[carID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
@@ -47,7 +47,7 @@ GO
 USE [RentalProject]
 GO
 
-/****** Object:  Table [dbo].[Pricing]    Script Date: 11/30/2018 12:49:15 AM ******/
+/****** Object:  Table [dbo].[Pricing]    Script Date: 11/30/2018 1:47:52 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -55,22 +55,27 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[Pricing](
+	[pricingID] [nvarchar](50) NOT NULL,
 	[carID] [nvarchar](50) NULL,
-	[rate] [decimal](18, 2) NULL
+	[rate] [decimal](18, 2) NULL,
+ CONSTRAINT [PK_Pricing] PRIMARY KEY CLUSTERED 
+(
+	[pricingID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[Pricing]  WITH CHECK ADD  CONSTRAINT [FK_Pricing_Cars] FOREIGN KEY([carID])
-REFERENCES [dbo].[Cars] ([carID])
+ALTER TABLE [dbo].[Pricing]  WITH CHECK ADD  CONSTRAINT [FK_Pricing_Car] FOREIGN KEY([carID])
+REFERENCES [dbo].[Car] ([carID])
 GO
 
-ALTER TABLE [dbo].[Pricing] CHECK CONSTRAINT [FK_Pricing_Cars]
+ALTER TABLE [dbo].[Pricing] CHECK CONSTRAINT [FK_Pricing_Car]
 GO
 
 USE [RentalProject]
 GO
 
-/****** Object:  Table [dbo].[Reservation]    Script Date: 11/30/2018 12:49:31 AM ******/
+/****** Object:  Table [dbo].[Reservation]    Script Date: 11/30/2018 1:48:03 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -78,18 +83,23 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[Reservation](
+	[resID] [nvarchar](50) NOT NULL,
 	[userID] [nvarchar](50) NULL,
 	[carID] [nvarchar](50) NULL,
-	[fromDate] [date] NULL,
-	[toDate] [date] NULL
+	[fromDate] [datetime] NULL,
+	[toDate] [datetime] NULL,
+ CONSTRAINT [PK_Reservation] PRIMARY KEY CLUSTERED 
+(
+	[resID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[Reservation]  WITH CHECK ADD  CONSTRAINT [FK_Reservation_Cars] FOREIGN KEY([carID])
-REFERENCES [dbo].[Cars] ([carID])
+ALTER TABLE [dbo].[Reservation]  WITH CHECK ADD  CONSTRAINT [FK_Reservation_Car] FOREIGN KEY([carID])
+REFERENCES [dbo].[Car] ([carID])
 GO
 
-ALTER TABLE [dbo].[Reservation] CHECK CONSTRAINT [FK_Reservation_Cars]
+ALTER TABLE [dbo].[Reservation] CHECK CONSTRAINT [FK_Reservation_Car]
 GO
 
 ALTER TABLE [dbo].[Reservation]  WITH CHECK ADD  CONSTRAINT [FK_Reservation_UserAccount] FOREIGN KEY([userID])
